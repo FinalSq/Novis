16fad07a3847ccf3d2a611bd875448dd907e14a8
FinalSq <FinalSq1@hotmail.com>
2021-08-26 23:02:20 -0700

    Update app.html, site.js and code.html
    Updated content in the code.html and app.html. Changed some of the site.js code to better clean it up after watching the CF tutorial.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,10 @@
 // On button push, runs logic to reverse the string
 function operationReverse() {
 
+    // Makes reveal section invisible when this program starts
+    document.getElementById("reveal").classList.add("invisible");
+
+    // Runs the functions needed to perform the logic for the program
     let userString = getString();
     let reversedString = reverseString(userString);
 
@@ -19,12 +23,14 @@ function getString() {
 }
 
 // Reverse the string and return the reversed string
-function reverseString(normalString) {
+function reverseString(userString) {
 
-    let stringToBeReversed = normalString;
-    let reversedString = "";
-    for (let index = stringToBeReversed.length - 1; index >= 0; index--) {
-        reversedString = reversedString + stringToBeReversed[index];
+    // Creates a variable with type array
+    let reversedString = [];
+
+    // Iterates backwards through the array "stringToBeReversed" and concatenates it with reversedString
+    for (let index = userString.length - 1; index >= 0; index--) {
+        reversedString += userString[index];
     }
 
     return reversedString;
@@ -32,25 +38,21 @@ function reverseString(normalString) {
 
 // Reveal the reversed string on display
 function revealOnDisplay(reversedString) {
-    let reveal = document.getElementById("reveal");
+    
+    // Adds a string to a <p> tag in the ID #reveal
+    let message = document.getElementById("msg");
+    message.innerHTML = `Your string reversed is: ${reversedString}`;
 
-    reveal.innerHTML = `
-    <div class="border rounded mt-5 opacity p-4">
-        <div class="border-bottom border-2 borderLRed">
-            <h1 class="darkRed"><strong>Well done!</strong></h1>
-        </div>
-        <div>
-            <h5 class="mt-3">Your string reversed is: ${reversedString}</h5>
-        </div>
-    </div>
-    `;
+    // Removes the "invisible" class and reveals the content in ID #reveal
+    document.getElementById("reveal").classList.remove("invisible");
 }
 
-//                   <div class="border rounded mt-5 opacity p-4">
-//                      <div class="border-bottom border-2 borderLRed">
-//                           <h1 class="darkRed"><strong>Well done!</strong></h1>
-//                       </div>
-//                       <div>
-//                           <h5 class="mt-3">Your string reversed is: </span></h5>
-//                       </div>
-//                   </div>
+// Clears content created by revealOnDisplay() and empties the user input box
+function clearContent(){
+
+    let userString = document.getElementById("userString");
+    userString.value = "";
+
+    document.getElementById("reveal").classList.add("invisible");
+}
+
